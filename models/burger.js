a60becd25a6b4e16307ae58f8cd562cc3e087c0b
jilljurgens <jurgensjill@gmail.com>
2018-01-05 19:39:47 -0600

    more changes to routers

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,23 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
+
 var burger = {
   selectAll: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-//   create: function(cols, vals, cb) {
-//     orm.create("burgers", cols, vals, function(res) {
-//       cb(res);
-//     });
-//   },
-  insertOne: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  updateOne: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
 };
-// Export the database functions for the controller (catsController.js).
-module.exports = burgers;
+// Export the database functions for the controller (burgers_controllers.js).
+module.exports = burger;
